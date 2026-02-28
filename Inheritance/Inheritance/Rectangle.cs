695ec9193d3f8d1d8fc2b6ccda4b6e0363aefabb
NickGames2 <jlagudelo5@gmail.com>
2026-02-28 09:59:23 -0500

    Class Rhombus

---
+++ Inheritance/Inheritance/Rectangle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public class Rectangle : Square
+    {
+        private double _b;
+
+        public Rectangle(double sideA, double sideB) : base(sideA)
+        {
+            A = sideA;
+            B = sideB;
+            Name = "Rectangle";
+        }
+        public double B
+        {
+            get
+            {
+                return _b;
+            }
+            set
+            {
+                if (ValidateB(value))
+                {
+                    _b = value;
+                }
+            }
+
+        }
+
+        private bool ValidateB(double B)
+        {
+            return B > 0;
+        }
+
+        public override double GetArea() => A * B;
+
+        public override double GetPerimeter() => 2 * (A + B);
+    }
+}
---
+++ Inheritance/Inheritance/Rhombus.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public class Rhombus : Square
+    {
+        private double _d1;
+        private double _d2;
+
+        public Rhombus(double d1, double d2, double sideA) : base(sideA)
+        {
+            A = sideA;
+            D1 = d1;
+            D2 = d2;
+            Name = "Rhombus";
+        }
+        public double D1
+        {
+            get
+            {
+                return _d1;
+            }
+            set
+            {
+                if (ValidateD1(value))
+                {
+                    _d1 = value;
+                }
+            }
+        }
+        public double D2
+        {
+            get
+            {
+                return _d2;
+            }
+
+            set
+            {
+                if (ValidateD2(value))
+                {
+                    _d2 = value;
+                }
+            }
+        }
+
+        private bool ValidateD1(double D1)
+        {
+            return D1 > 0;
+        }
+
+        private bool ValidateD2(double D2)
+        {
+            return D2 > 0;
+        }
+
+        public override double GetArea() => (D1 * D2) / 2;
+
+        public override double GetPerimeter() => 4 * A;
+
+
+    }
+}
