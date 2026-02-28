4336029d2a11155f666e32f72d79b5ace31aa314
NickGames2 <jlagudelo5@gmail.com>
2026-02-28 10:11:01 -0500

    Class Trapece

---
+++ Inheritance/Inheritance/Trapece.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public class Trapece : Triangle
+    {
+        private double _d;
+
+        public Trapece(double sideA, double sideB, double sideC, double sideD, double h) : base(h, sideA, sideB, sideC)
+        {
+            A = sideA;
+            B = sideB;
+            C = sideC;
+            D = sideD;
+            H = h;
+            Name = "Trapece";
+        }
+        public double D
+        {
+            get
+            {
+                return _d;
+            }
+            set
+            {
+                if (ValidateD(value))
+                {
+                    _d = value;
+                }
+            }
+        }
+
+        public override double GetArea() => (B + D) * H / 2;
+
+        public override double GetPerimeter() => A + B + C + D;
+
+        private bool ValidateD(double D)
+        {
+            return D > 0;
+        }
+
+    }
+}
