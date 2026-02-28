b0f35bf7ea2928c5710083b05c1b5fd7267ecf5a
NickGames2 <jlagudelo5@gmail.com>
2026-02-28 08:58:46 -0500

    Class Square

---
+++ Inheritance/Inheritance/GeometricFIgure.cs
@@ -0,0 +1,16 @@
+﻿namespace Inheritance
+{
+
+        public abstract class GeometricFigure
+        {
+            public string Name { get; set; } = null!;
+
+            public override string ToString()
+            {
+                return $"{Name,-15} => Area.....: {GetArea(),12:N5}   Perimeter: {GetPerimeter(),12:N5}";
+            }
+            public abstract double GetArea();
+            public abstract double GetPerimeter();
+        }
+    
+}
---
+++ Inheritance/Inheritance/Square.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public class Square : GeometricFigure
+    {
+        private double _a;
+
+        public double A
+        {
+            get { return _a; }
+            set
+            {
+                if (ValidateA(value))
+                {
+                    _a = value;
+                }
+            }
+        }
+
+        public Square(double side)
+        {
+            A = side;
+            Name = "Square";
+        }
+
+        private bool ValidateA(double value)
+        {
+            return value > 0;
+        }
+
+        public override double GetArea()
+        {
+            return _a * _a;
+        }
+
+        public override double GetPerimeter()
+        {
+            return 4 * _a;
+        }
+    }
+}
